e989353c3c1bba73df4f9c42e706028c7abc674f
ArtemT <artemt-2005@mail.ru>
2021-01-11 20:09:52 +0500

    Note class

---
+++ TaggedNotes.View/Note.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+using TaggedNotes.Interfaces;
+
+namespace TaggedNotes.View
+{
+    /// <summary>
+    /// Note realization
+    /// </summary>
+    class Note : BaseObject, INote, INotifyPropertyChanged
+    {
+        #region INote contract
+        public string Text { get; set; }
+
+        public ObservableCollection<ITag> Tags { get; set; }
+        #endregion
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="id">Object identifier</param>
+        /// <param name="name">Tag name</param>
+        /// <param name="selected">Is tag checked</param>
+        public Note(int id, string text, ObservableCollection<ITag> tags)
+        {
+            Id = id;
+            Text = text;
+            Tags = tags;
+        }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+
+        #region INotifyPropertyChanged contract
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+    }
+}
