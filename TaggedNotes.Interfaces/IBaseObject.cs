d22d1628da4156201089e8258da03ea5fb05922e
ArtemT <artemt-2005@mail.ru>
2021-01-11 20:09:05 +0500

    Interface for base objects

---
+++ TaggedNotes.Interfaces/IBaseObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaggedNotes.Interfaces
+{
+    /// <summary>
+    /// Base interface for all objects
+    /// </summary>
+    public interface IBaseObject
+    {
+        /// <summary>
+        /// Object identifier
+        /// </summary>
+        int Id { get; set; }
+    }
+}
