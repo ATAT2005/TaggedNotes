67e19dd4aecab020fc62c30bdc8222916173ffc1
ArtemT <artemt-2005@mail.ru>
2021-01-11 20:09:33 +0500

    Interface for tag

---
+++ TaggedNotes.Interfaces/ITag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaggedNotes.Interfaces
+{
+    /// <summary>
+    /// Tag interface
+    /// </summary>
+    public interface ITag : IBaseObject
+    {
+
+        /// <summary>
+        /// Tag name
+        /// </summary>
+        string Name { get; set; }
+        
+        /// <summary>
+        /// Is tag checked
+        /// </summary>
+        bool IsSelected { get; set; }
+    }
+}
