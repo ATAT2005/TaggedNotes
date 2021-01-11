b0727864e3573681fab470c2c79fef669648efd5
ArtemT <artemt-2005@mail.ru>
2021-01-11 20:09:16 +0500

    Base object class

---
+++ TaggedNotes.View/BaseObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using TaggedNotes.Interfaces;
+
+namespace TaggedNotes.View
+{
+    /// <summary>
+    /// Base object realization
+    /// </summary>
+    abstract class BaseObject : IBaseObject
+    {
+        #region IBaseObject contract
+        public int Id { get; set; }
+        #endregion
+    }
+}
