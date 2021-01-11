eb7ceec3638da9d073fc57131c832bf88ee862c2
ArtemT <artemt-2005@mail.ru>
2021-01-11 20:09:41 +0500

    Interface for note

---
+++ TaggedNotes.Interfaces/INote.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace TaggedNotes.Interfaces
+{
+    /// <summary>
+    /// Note interface
+    /// </summary>
+    public interface INote : IBaseObject
+    {
+        /// <summary>
+        /// Note text
+        /// </summary>
+        string Text { get; set; }
+
+        /// <summary>
+        /// Tags linked to note
+        /// </summary>
+        ObservableCollection<ITag> Tags { get; set; }
+    }
+}
