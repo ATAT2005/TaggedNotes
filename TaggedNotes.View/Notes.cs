a28e9a1a6df2d7eab48a6d27bd220c9b80e46914
ArtemT <artemt-2005@mail.ru>
2021-01-11 20:10:25 +0500

    Note collection class

---
+++ TaggedNotes.View/Notes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+using TaggedNotes.Interfaces;
+
+namespace TaggedNotes.View
+{
+    public class Notes : ObservableCollection<INote>
+    {
+        public Notes()
+        {
+            this.Add(new Note(1, "Note 1", null));
+            this.Add(new Note(2, "Note 222", null));
+        }
+    }
+}
