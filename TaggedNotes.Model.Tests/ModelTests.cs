fe45d7420065995598096f212bdfe2212c4b60db
ArtemT <artemt-2005@mail.ru>
2021-03-29 20:24:10 +0500

    Unit test projects

---
+++ TaggedNotes.Model.Tests/ModelTests.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using TaggedNotes.Model;
+
+namespace TaggedNotes.Model.Tests
+{
+	public class ModelTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void TestTag()
+		{
+			var tag = new Tag(1, "test", true);
+
+			Assert.AreEqual(tag.Id, 1);
+			Assert.AreEqual(tag.Name, "test");
+			Assert.IsTrue(tag.Selected);
+			Assert.AreEqual(tag.ToString(), "test");
+		}
+
+		[Test]
+		public void TestNote()
+		{
+			var tag = new Tag(1, "test", true);
+
+			var note = new Note(1, "noteText", new System.Collections.ObjectModel.ObservableCollection<Interfaces.ITag>(new Tag[] { tag }));
+
+			Assert.AreEqual(note.Id, 1);
+			Assert.AreEqual(note.Text, "noteText");
+			Assert.AreEqual(note.Tags, new List<Interfaces.ITag>(new[] { tag }));
+			Assert.AreEqual(note.ToString(), "noteText");
+		}
+	}
+}
---
+++ TaggedNotes.ViewModel.Tests/ViewModelTests.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+
+using TaggedNotes.Model;
+using TaggedNotes.ViewModel;
+
+namespace TaggedNotes.ViewModel.Tests
+{
+	public class ViewModelTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void TestViewModel()
+		{
+			var vm = new TaggedNotesViewModel();
+			Assert.IsNotNull(vm);
+			vm.Tags.Clear();
+			vm.Notes.Clear();
+
+			var selectedNode = vm.SelectedNote;
+			Assert.IsNull(selectedNode);
+
+			var tag = new Tag(1, "test", false);
+
+			vm.AddTag(tag);
+			Assert.AreEqual(vm.Tags.Count, 1);
+			
+			vm.RemoveTag(tag);
+			Assert.AreEqual(vm.Tags.Count, 0);
+
+			var note = new Note(1, "text", new System.Collections.ObjectModel.ObservableCollection<Interfaces.ITag>(new Tag[] { tag }));
+			vm.AddNote(note);
+			Assert.AreEqual(vm.Notes.Count, 1);
+
+			vm.RemoveNote(note);
+			Assert.AreEqual(vm.Tags.Count, 0);
+		}
+	}
+}
