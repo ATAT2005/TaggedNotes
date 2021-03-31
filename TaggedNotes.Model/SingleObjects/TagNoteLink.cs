eb72c0275292ae92432aaeae6abcb9108765c5eb
ArtemT <artemt-2005@mail.ru>
2021-03-31 20:31:55 +0500

    New class: link between tag and note

---
+++ TaggedNotes.Model/SingleObjects/TagNoteLink.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaggedNotes.Model
+{
+	public class TagNoteLink
+	{
+		public int IdTag { get; set; }
+
+		public Tag Tag { get; set; }
+
+		public int IdNote{ get; set; }
+
+		public Note Note { get; set; }
+
+		/// <summary>
+		/// Empty constructor (for EntityFramework, etc)
+		/// </summary>
+		public TagNoteLink() { }
+
+		public TagNoteLink(Note note, Tag tag)
+		{
+			Note = note;
+			Tag = tag;
+			IdTag = tag.Id;
+			IdNote = note.Id;
+		}
+	}
+}
