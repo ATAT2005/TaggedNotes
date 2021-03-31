3ad040e2e4c2535589ae42d2fdd383d1c153f84b
ArtemT <artemt-2005@mail.ru>
2021-03-31 20:22:40 +0500

    ViewModel class file was renamed

---
+++ TaggedNotes.ViewModel/ViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TaggedNotes.Interfaces;
+using TaggedNotes.Model;
+
+namespace TaggedNotes.ViewModel
+{
+	public class ViewModel
+	{
+		public INote SelectedNote { get; set; }
+
+		public ObservableCollection<ITag> Tags { get; set; }
+		
+		public ObservableCollection<INote> Notes { get; set; }
+
+		public ViewModel()
+		{
+			Tags = new ObservableCollection<ITag>();
+
+			Notes = new ObservableCollection<INote>();
+
+			using (var context = new ModelContext())
+			{
+				foreach (var note in context.Notes)
+					Notes.Add(note);
+
+				foreach (var tag in context.Tags)
+					Tags.Add(tag);
+			}
+		}
+
+		/*public void AddTag(ITag tag)
+		{
+			Tags.Add(tag);
+		}
+
+		public void RemoveTag(ITag tag)
+		{
+			Tags.Remove(tag);
+		}
+
+		public void AddNote(INote note)
+		{
+			Notes.Add(note);
+		}
+
+		public void RemoveNote(INote note)
+		{
+			Notes.Remove(note);
+		}*/
+	}
+}
