05c979c68a1cb55bf30a4b97bb4dcd861aa2e4d4
ArtemT <artemt-2005@mail.ru>
2021-04-20 21:13:45 +0500

    ListBoxes are replaced by DataGrids and they use text filters now

--- TaggedNotes.View/View.xaml.cs
+++ TaggedNotes.View/View.xaml.cs
@@ -18,61 +18,107 @@ using System.Windows.Shapes;
 
 using TaggedNotes.Interfaces;
 using TaggedNotes.ViewModel;
+using TaggedNotes.Model;
 
 namespace TaggedNotes.View
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class TaggedNotesMainWindow : Window
-    {
-        public TaggedNotesMainWindow()
-        {
-            InitializeComponent();
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class TaggedNotesMainWindow : Window
+	{
+		private ViewModel.ViewModel VM { get => DataContext as ViewModel.ViewModel; }
 
-            AddHandler(CheckBox.PreviewMouseLeftButtonDownEvent, new RoutedEventHandler(tag_PreviewMouseLeftButtonDownEvent));
+		CollectionViewSource _viewSourceNotes;
+
+		CollectionViewSource _viewSourceTags;
+
+		public TaggedNotesMainWindow()
+		{
+			InitializeComponent();
+
+			AddHandler(CheckBox.PreviewMouseLeftButtonDownEvent, new RoutedEventHandler(tag_PreviewMouseLeftButtonDownEvent));
 			//AddHandler(ListBox.SelectionChangedEvent, new RoutedEventHandler(List_SelectionChanged));
 
 			DataContext = new ViewModel.ViewModel();
-        }
+		}
 
-        /// <summary>
-        /// Special workaround for stretching short textbox items of the listbox
-        /// </summary>
-        /// <see cref="https://social.msdn.microsoft.com/Forums/security/en-US/d066ea21-2723-4622-8276-698c745f4184/how-to-make-textbox-stretch-horizontally-to-fill-width-of-listbox?forum=silverlightnet"/>
-        private void Grid_Loaded(object sender, RoutedEventArgs e)
-        {
-            FrameworkElement g = sender as FrameworkElement;
-            g.Width = ListNotes.ActualWidth - 6;  // make the grid the same width as the listbox (- some border width)
-        }
+		/// <summary>
+		/// Special workaround for stretching short textbox items of the listbox
+		/// </summary>
+		/// <see cref="https://social.msdn.microsoft.com/Forums/security/en-US/d066ea21-2723-4622-8276-698c745f4184/how-to-make-textbox-stretch-horizontally-to-fill-width-of-listbox?forum=silverlightnet"/>
+		private void Grid_Loaded(object sender, RoutedEventArgs e)
+		{
+			if (DataGridNotes.Items.Count > 0)
+				DataGridNotes.SelectedItem = DataGridNotes.Items[0];
 
-        private void List_SelectionChanged(object sender, RoutedEventArgs args)
-        {
-            var viewmodel = DataContext as ViewModel.ViewModel;
+			_viewSourceNotes = new CollectionViewSource() { Source = VM.Notes };
+			_viewSourceNotes.Filter += new FilterEventHandler(FilterNotes);
+			ICollectionView notesList = _viewSourceNotes.View;
+			DataGridNotes.ItemsSource = notesList;
 
-            if (viewmodel == null)
-                return;
+			_viewSourceTags = new CollectionViewSource() { Source = VM.Tags };
+			_viewSourceTags.Filter += new FilterEventHandler(FilterTags);
+			ICollectionView tagsList = _viewSourceTags.View;
+			DataGridTags.ItemsSource = tagsList;
 
-            //var tags = viewmodel.SelectedNote.TagNoteLinks;
+			//FrameworkElement g = sender as FrameworkElement;
+			//g.Width = ListNotes.ActualWidth - 6;  // make the grid the same width as the listbox (- some border width)
+		}
 
-            int i = 0;
-        }
+		private void FilterNotes(object sender, FilterEventArgs e)
+		{
+			var note = e.Item as Note;
 
-        /// <summary>
-        /// Start dragging a tag on a note
-        /// </summary>
-        private void tag_PreviewMouseLeftButtonDownEvent(object sender, RoutedEventArgs e)
-        {
-            if (e.OriginalSource is FrameworkElement fe && fe.DataContext is ITag dc)
-                DragDrop.DoDragDrop(fe, dc, DragDropEffects.Copy);
-        }
+			if (note != null && !string.IsNullOrWhiteSpace(NoteFilter.Text))
+				e.Accepted = note.Text.Contains(NoteFilter.Text, StringComparison.OrdinalIgnoreCase);
+		}
 
-        /// <summary>
-        /// Dropping a tag on a note to make a link with it
-        /// </summary>
-        private void note_Drop(object sender, DragEventArgs e)
-        {
+		private void FilterTags(object sender, FilterEventArgs e)
+		{
+			var tag = e.Item as Tag;
 
-        }
-    }
+			if (tag != null && !string.IsNullOrWhiteSpace(TagFilter.Text))
+				e.Accepted = tag.Name.Contains(TagFilter.Text, StringComparison.OrdinalIgnoreCase);
+		}
+
+		private void noteFilter_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			_viewSourceNotes.View.Refresh();
+		}
+
+		private void tagFilter_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			_viewSourceTags.View.Refresh();
+		}
+
+		private void List_SelectionChanged(object sender, RoutedEventArgs args)
+		{
+			//var viewmodel = DataContext as ViewModel.ViewModel;
+
+			//if (viewmodel == null)
+			//	return;
+
+			//var tags = viewmodel.SelectedNote.TagNoteLinks;
+
+			//int i = 0;
+		}
+
+		/// <summary>
+		/// Start dragging a tag on a note
+		/// </summary>
+		private void tag_PreviewMouseLeftButtonDownEvent(object sender, RoutedEventArgs e)
+		{
+			if (e.OriginalSource is FrameworkElement fe && fe.DataContext is ITag dc)
+				DragDrop.DoDragDrop(fe, dc, DragDropEffects.Copy);
+		}
+
+		/// <summary>
+		/// Dropping a tag on a note to make a link with it
+		/// </summary>
+		private void note_Drop(object sender, DragEventArgs e)
+		{
+
+		}
+	}
 }
